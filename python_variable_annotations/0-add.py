7f5f8f78f34ce9678f3060a0dc446ab187427128
JustGodWork <discord1elie@gmail.com>
2024-04-22 10:37:29 +0200

    Added task 0 (Python - Variable Annotations)

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Simple add function using two floats
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        Calculate addition of value
+        a(float) & b(float) and return the result
+    """
+    return a + b
