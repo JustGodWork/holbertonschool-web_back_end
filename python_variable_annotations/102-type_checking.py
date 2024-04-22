dad1a3cd5df05fb20445264324211b04eab32508
JustGodWork <discord1elie@gmail.com>
2024-04-22 11:08:26 +0200

    Added last tasks (Python - Variable Annotations)

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    Simple type checking function
+"""
+
+
+from typing import Tuple, Any, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+        Zoom a list
+        lst(Tuple) & factor(int) and return the result
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
