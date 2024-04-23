52cebcb6d118a8558b3379775b1b8b3d62a08e41
JustGodWork <discord1elie@gmail.com>
2024-04-23 09:42:58 +0200

    Removed type ignore due to coding style

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:
     """Yield a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
