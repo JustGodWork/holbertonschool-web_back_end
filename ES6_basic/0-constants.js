983f9ae2706a34ab5e1bcbbb279bf77b8ebb6b7a
JustGodWork <discord1elie@gmail.com>
2024-05-06 11:46:45 +0200

    Fix task 0 (ES6 Basics)

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,24 +1,12 @@
-/**
- * Create a constant named task and return it
- * @returns {string} task
- */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-/**
- * Create an anonymous string and return it
- * @returns {string} task
- */
 export function getLast() {
   return ' is okay';
 }
 
-/**
- * Create an editable string and return it
- * @returns {string} task
- */
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
