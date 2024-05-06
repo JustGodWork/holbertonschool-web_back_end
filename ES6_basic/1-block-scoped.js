5cc23faaa9505af41de2d2efccc6bf0bd7622069
JustGodWork <discord1elie@gmail.com>
2024-05-06 12:04:22 +0200

    Added task 1 (ES6 Basics)

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,10 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-disable */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
+  /* eslint-enable */
 
   return [task, task2];
 }
