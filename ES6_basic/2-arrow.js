eca2167a13b795efe060c01cdfb1d5e4210af433
JustGodWork <discord1elie@gmail.com>
2024-05-06 12:07:13 +0200

    Added task 2 (ES6 Basics)

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+const getNeighborhoodsList = () => {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+};
+
+export default getNeighborhoodsList;
