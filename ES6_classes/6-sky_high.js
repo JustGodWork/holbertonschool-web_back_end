1d6e14cbb047241d4d7edc73168c480e4fd58568
root <root@JUSTGOD>
2024-05-11 16:18:22 +0200

    Added tasks (6) (ES6 classes)

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -16,6 +16,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors.`;
   }
 }
