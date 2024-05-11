a9f1449acd7ce6dc2d8f078ef3e7b1afe9793a5c
JustGodWork <discord1elie@gmail.com>
2024-05-11 15:56:39 +0200

    Added tasks (5) (ES6 classes)

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,1 +1,13 @@
 /* eslint-disable */
+export default class Building {
+    constructor(sqft) {
+        this._sqft = sqft;
+        if (this.constructor !== Building && !this.evacuationWarningMessage) {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+}
