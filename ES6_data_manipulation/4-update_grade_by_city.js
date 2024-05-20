bc06cfaade6fef06df354d331df87b0cd79081b9
JustGodWork <discord1elie@gmail.com>
2024-05-20 07:27:25 +0200

    Fix task 4 (ES6 Data Manipulation)

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students.map((student) => {
-    if (student.location === city) {
-      const updatedGrade = newGrades.find((grade) => grade.studentId === student.id);
-      if (updatedGrade) return { ...student, grade: updatedGrade.grade };
+  return students.filter((student) => student.location === city).map((student) => {
+    const updatedGrade = newGrades.find((grade) => grade.studentId === student.id);
+    if (updatedGrade) {
+      return { ...student, grade: updatedGrade.grade };
     }
-    return student;
+    return { ...student, grade: 'N/A' };
   });
 }
