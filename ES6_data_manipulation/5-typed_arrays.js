f8c7d0e9be552611ce941abf1001704afe1b0fce
JustGodWork <discord1elie@gmail.com>
2024-05-20 07:28:35 +0200

    Fix task 5 (ES6 Data Manipulation)

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
 
   if (position < 0 || position >= length) throw new Error('Position outside range');
 
-  view[position] = value;
+  const view = new DataView(buffer);
 
-  return buffer;
+  view.setInt8(position, value);
+  return view;
 }
