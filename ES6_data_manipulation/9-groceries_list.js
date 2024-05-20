37d0e0571cb2f2deec8d9152a00cf6f1f919fccd
JustGodWork <discord1elie@gmail.com>
2024-05-20 07:42:49 +0200

    Fix task 9 (ES6 Data Manipulation)

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,11 @@
 export default function groceriesList() {
-  return new Map([
+  const groceries = new Map([
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
   ]);
-}
 
+  return groceries;
+}
