52bac7182ed0914b17dd6d127cf82acf9b45a546
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-01-28 04:37:40 +0100

    Refactor filter_datum function for improved readability by adjusting parameter formatting

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,11 +9,13 @@ import mysql.connector
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     for field in fields:
         message = re.sub(rf"{field}=.+?{separator}",
                          f"{field}={redaction}{separator}", message)
     return message
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class """
