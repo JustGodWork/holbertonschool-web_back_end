c957745dcf7d10babd6ee11bce7cfd82bffe4a23
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-01-28 04:20:28 +0100

    Add main function to retrieve and log user data from the database

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -54,3 +54,23 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         host=host,
         database=database
     )
+
+
+def main():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+
+    for row in cursor:
+        message = f"name={row[0]}; email={row[1]};\
+              phone={row[2]}; ssn={row[3]}; password={row[4]};\
+                  ip={row[5]}; last_login={row[6]}; user_agent={row[7]}"
+        logger.info(message)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
