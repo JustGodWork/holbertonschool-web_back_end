4c5531cec6411ebeaa3bb42c518bd4eb5d6c3bfc
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-01-28 04:22:34 +0100

    Add password validation function to enhance security

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    return bcrypt.checkpw(password.encode(), hashed_password)
