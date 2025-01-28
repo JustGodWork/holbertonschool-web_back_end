d14a11d8ca7ed4c5e4184f5369f4b105846d4fea
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-01-28 04:39:00 +0100

    Refactor get_logger function to improve formatting of RedactingFormatter instantiation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -46,9 +46,11 @@ def get_logger() -> logging.Logger:
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
+
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(stream_handler)
+
     return logger
 
 
