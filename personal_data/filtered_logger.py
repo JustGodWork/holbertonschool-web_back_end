8b515cb250c07553913f74b0d24c6187980fdc46
JustGod <85418813+JustGodWork@users.noreply.github.com>
2025-01-28 04:15:58 +0100

    Add get_logger function to initialize a logger with PII redaction

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -25,3 +25,16 @@ class RedactingFormatter(logging.Formatter):
         original_message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             original_message, self.SEPARATOR)
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
