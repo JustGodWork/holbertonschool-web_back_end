0b3cee6bb91fbf2b91894613ecb09cf2f950698d
= <=>
2025-02-19 11:40:09 +0100

    Update app.py to support basic authentication by importing BasicAuth when AUTH_TYPE is set to 'basic_auth'

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -13,7 +13,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 
-if getenv("AUTH_TYPE") == "auth":
+if getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
