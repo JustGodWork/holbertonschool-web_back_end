6d58ed537db5c883cc3a80547ba6962244ae1f25
= <=>
2025-02-19 11:30:05 +0100

    Refactor Auth class by removing unnecessary blank line and adjusting comment formatting

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,6 @@ from typing import List, TypeVar
 class Auth:
     """  Auth class """
 
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method to check if authentication is required """
         return False
@@ -17,6 +16,6 @@ class Auth:
         """ Method to get the authorization header """
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """ Method to get the current user """
         return None
