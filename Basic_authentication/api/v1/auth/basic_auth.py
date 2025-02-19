9676dc04e1e950b56be5f0d114c4cd50c1c0dc67
= <=>
2025-02-19 11:42:40 +0100

    Add method to extract Base64 part of Authorization header in BasicAuth class

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ BasicAuth class """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extracts the Base64 part of the Authorization header """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ", 1)[1]
