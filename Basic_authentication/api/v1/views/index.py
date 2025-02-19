788b32268493adab565bef675d93253c17e3805f
= <=>
2025-02-19 11:17:22 +0100

    Add unauthorized endpoint to trigger 401 error

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint():
+    """ Endpoint to raise a 401 error
+    """
+    abort(401)
