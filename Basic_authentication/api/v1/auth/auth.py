9da75aae24743251ed192cec112a8212e1e014a7
= <=>
2025-02-19 11:33:05 +0100

    Update require_auth method to handle excluded_paths cases

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,15 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method to check if authentication is required """
-        return False
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        path = path.rstrip('/') + '/'
+        for excluded_path in excluded_paths:
+            if excluded_path.rstrip('/') + '/' == path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to get the authorization header """
