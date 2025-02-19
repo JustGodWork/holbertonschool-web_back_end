b84c1bdcb4f1073ef8521171f96395d8725f8650
= <=>
2025-02-19 11:36:00 +0100

    Implement request filtering in the API with authentication checks

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -4,13 +4,36 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask_cors import (CORS)
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+
+if getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request_handler():
+    """ Method to handle before request filtering """
+    if auth is None:
+        return
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
