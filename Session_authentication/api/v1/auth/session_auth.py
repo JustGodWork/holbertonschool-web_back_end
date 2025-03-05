aa73edbd3ece9c0111d62ba38a05ec9e3ea1b5b8
JustGodWork <discord1elie@gmail.com>
2025-03-05 16:42:36 +0100

    Improve docstring formatting in SessionAuth class for clarity

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""Session Authentication module"""
+""" Session Authentication module """
 
 
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
+    """ SessionAuth class """
     pass
