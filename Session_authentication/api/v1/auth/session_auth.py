c5ec6f5b003e451ed757cd5830b389c24e4d1492
JustGodWork <discord1elie@gmail.com>
2025-03-05 17:01:22 +0100

    Add current_user method to retrieve User instance from session cookie

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 
 import uuid
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -22,3 +23,11 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Return a User instance based on a cookie value """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id:
+            return User.get(user_id)
+        return None
