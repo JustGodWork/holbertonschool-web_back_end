b63f7f7a1b11c3d818528b642e9b8ae4528ef6cd
JustGodWork <discord1elie@gmail.com>
2025-03-05 17:21:41 +0100

    Refactor login route definition for improved readability

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -6,8 +6,7 @@ from models.user import User
 from os import getenv
 
 
-@app_views.route('/auth_session/login', methods=['POST'],
-                    strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
     """ Handle login route for session authentication """
     from api.v1.app import auth
