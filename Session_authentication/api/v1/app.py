69468b6f82104e9f664e5b5b46fef860f05c4b1f
JustGodWork <discord1elie@gmail.com>
2025-03-05 16:41:12 +0100

    Refactor authentication initialization to streamline imports and improve clarity

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -7,6 +7,10 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, request, abort
 from flask_cors import (CORS)
 
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -16,13 +20,10 @@ auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 
 if AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 elif AUTH_TYPE == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 else:
-    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
