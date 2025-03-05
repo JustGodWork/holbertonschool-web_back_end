00f880f1d63ec426dd6b64918ab6ccb51ef116d0
JustGodWork <discord1elie@gmail.com>
2025-03-05 16:47:30 +0100

    Implement session creation in SessionAuth class with UUID generation

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 """ Session Authentication module """
 
-
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """ SessionAuth class """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a Session ID for a user_id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
