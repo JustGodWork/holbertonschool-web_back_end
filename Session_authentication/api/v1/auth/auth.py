65aa30a1d9f916a083e58c7f54f822cf4b809bee
JustGodWork <discord1elie@gmail.com>
2025-03-05 16:53:56 +0100

    Add method to retrieve session cookie from request in Auth class

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -31,3 +32,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """ Method to get the current user """
         return None
+
+    def session_cookie(self, request=None):
+        """ Return a cookie value from a request """
+        if request is None:
+            return None
+        session_name = getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
