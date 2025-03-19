1115ff049b970ec84f571388c53de61e81c794ed
JustGodWork <discord1elie@gmail.com>
2025-03-19 22:23:15 +0100

    Add profile route to retrieve user information based on session ID

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -54,5 +54,15 @@ def logout():
     return redirect("/", 302)
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    """Retrieve user profile based on session_id."""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
