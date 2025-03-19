d78a319b2e164a4de37875ce60d711b9eb249e87
JustGodWork <discord1elie@gmail.com>
2025-03-19 22:25:22 +0100

    Add method to generate password reset token for user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -75,3 +75,13 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate a password reset token for the user."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
