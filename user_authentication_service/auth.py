6c9c8296571e1f8983a3d9e0e23983a69a5fc819
JustGodWork <discord1elie@gmail.com>
2025-03-19 22:28:26 +0100

    Add method to update user password using reset token

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -85,3 +85,13 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update the user's password using the reset token."""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError("Invalid reset token")
+        hashed_pw = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed_pw,
+                             reset_token=None)
