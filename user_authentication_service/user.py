87c2f97b87919e158db3fea518849f234575d60e
JustGodWork <discord1elie@gmail.com>
2025-03-19 22:26:42 +0100

    Make email field unique constraint optional and allow nullable session_id and reset_token fields

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -15,7 +15,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(250), unique=True, nullable=False)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250))
-    reset_token = Column(String(250))
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
