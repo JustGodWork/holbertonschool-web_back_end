420d51bb4ff7e5b01e68d1c3aea3a3b0fc9d8d94
JustGodWork <discord1elie@gmail.com>
2025-03-19 16:35:23 +0100

    Refactor DB class to improve docstring formatting and enhance add_user method parameter usage

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -15,8 +15,7 @@ class DB:
     """ Database class """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """ Initialize a new DB instance """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -24,17 +23,15 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """ Memoized session object """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database
-        """
-        user = User(email, hashed_password)
+        """ Add a user to the database """
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
