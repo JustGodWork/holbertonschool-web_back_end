59029d20b2c9c1c720c24e3d10516c5573935014
JustGodWork <discord1elie@gmail.com>
2025-03-19 22:27:42 +0100

    Add route to generate password reset token for user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -64,5 +64,16 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """Generate a password reset token for the user."""
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
