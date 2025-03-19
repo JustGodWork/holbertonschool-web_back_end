4bca540a1b40ddaddc4e804c8fd4f52cf3eeb58b
JustGodWork <discord1elie@gmail.com>
2025-03-19 22:32:08 +0100

    Refactor user authentication methods for clarity and error handling

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -3,7 +3,6 @@
 
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
@@ -32,39 +31,42 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add a user to the database """
+        """ Add a user to the DB"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         self._session.refresh(user)
         return user
 
-    def find_user_by(self, **kwargs: dict) -> User:
-        """Find a user by the given arbitrary keyword arguments."""
+    def find_user_by(self, **kwargs) -> User:
+        """
+            Finds the first user that matches the given filters.
+        """
 
         columns = User.__table__.columns.keys()
 
         for key in kwargs.keys():
             if key not in columns:
-                raise InvalidRequestError(f"User has no column {key}")
+                raise InvalidRequestError(f"Invalid column name: {key}")
 
         user = self._session.query(User).filter_by(**kwargs).first()
 
         if user is None:
-            raise NoResultFound("No user found with that criteria")
+            raise NoResultFound("No user found matching the criteria.")
 
         return user
 
-    def update_user(self, user_id: int, **kwargs: dict) -> None:
-        """Update a user's attributes by user_id."""
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ Update user parameters"""
         try:
             user = self.find_user_by(id=user_id)
+
             for key, value in kwargs.items():
                 if not hasattr(user, key):
-                    raise ValueError(
-                        f"User does not have that attribute: {key}"
-                    )
+                    raise ValueError(f"Invalide attribute: {key}")
                 setattr(user, key, value)
+
             self._session.commit()
+
         except NoResultFound:
-            raise NoResultFound(f"No user found with id {user_id}")
+            raise NoResultFound(f"User with id {user_id} not found")
