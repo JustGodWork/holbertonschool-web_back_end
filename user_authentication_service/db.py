afe9fb028511a8f76ea4548782bb4ee81516749c
JustGodWork <discord1elie@gmail.com>
2025-03-19 22:36:05 +0100

    Refactor database module docstrings for consistency and clarity

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
-""" Database module """
-
-
+"""DB module
+"""
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
-
 from user import Base, User
 
 
 class DB:
-    """ Database class """
+    """DB class
+    """
 
     def __init__(self) -> None:
-        """ Initialize a new DB instance """
+        """Initialize a new DB instance
+        """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -24,7 +25,8 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """ Memoized session object """
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
