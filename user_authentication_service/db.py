aef90fd4345468b5dabbd0efaa93e378826c764f
JustGodWork <discord1elie@gmail.com>
2025-03-19 16:40:32 +0100

    Add find_user_by method to DB class for user retrieval by keyword arguments

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 from user import Base, User
@@ -36,3 +38,13 @@ class DB:
         self._session.commit()
         self._session.refresh(user)
         return user
+
+    def find_user_by(self, **kwargs: dict) -> User:
+        """Find a user by the given arbitrary keyword arguments."""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except InvalidRequestError:
+            raise
