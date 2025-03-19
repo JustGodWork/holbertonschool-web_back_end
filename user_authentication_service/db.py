f8e97903f23cb7a9453ec2853939c0f11b0821c7
JustGodWork <discord1elie@gmail.com>
2025-03-19 16:27:47 +0100

    Add database module for user management with SQLAlchemy integration

---
+++ user_authentication_service/db.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Database module """
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+
+from user import Base, User
+
+
+class DB:
+    """ Database class """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, password: str) -> User:
+        """Add a user to the database
+        """
+        user = User(email=email, password=password)
+        self._session.add(user)
+        self._session.commit()
+        return user
