05e457f7cdbaf391edccb7e1b7d5e7b2a41120ad
JustGodWork <discord1elie@gmail.com>
2025-03-19 16:27:40 +0100

    Enhance User model with unique email constraint and initialization method

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -14,8 +14,14 @@ class User(Base):
 
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    email = Column(VARCHAR(250))
-    hashed_password = Column(VARCHAR(250))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(VARCHAR(250), unique=True, nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
     session_id = Column(VARCHAR(250))
     reset_token = Column(VARCHAR(250))
+
+    def __init__(self, email: str, password: str) -> None:
+        """ Initialize a new User instance
+        """
+        self.email = email
+        self.hashed_password = password
