3b0c23c58f46d50362f9e5eaf1e229554ce53c23
JustGodWork <discord1elie@gmail.com>
2025-03-19 17:07:23 +0100

    Add Auth class for user registration with password hashing

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,8 +3,29 @@
 
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Return a salted hash of the input password using bcrypt."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self) -> None:
+        """Initialize a new Auth instance."""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user if not already exist, else raise ValueError."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+        hashed_pw = _hash_password(password)
+        return self._db.add_user(email, hashed_pw)
