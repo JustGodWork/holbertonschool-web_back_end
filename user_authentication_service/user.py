eb498fe1fd304693f286adbb46bccc6b4c930e6d
JustGodWork <discord1elie@gmail.com>
2025-03-19 16:55:21 +0100

    Refactor User model to use String type instead of VARCHAR for column definitions

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """ User model """
 
 
-from sqlalchemy import Column, VARCHAR, Integer
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -14,8 +14,8 @@ class User(Base):
 
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(VARCHAR(250), unique=True, nullable=False)
-    hashed_password = Column(VARCHAR(250), nullable=False)
-    session_id = Column(VARCHAR(250))
-    reset_token = Column(VARCHAR(250))
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), unique=True, nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
