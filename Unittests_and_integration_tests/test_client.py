43b22a6256ac3a914a78149f65ae45628b590550
JustGodWork <discord1elie@gmail.com>
2025-03-27 11:50:19 +0100

    Add test for GithubOrgClient._public_repos_url method in test_client.py

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Unit tests for client.GithubOrgClient class.
 """
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import PropertyMock, patch
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -26,4 +26,22 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.return_value = expected
         client = GithubOrgClient(org_name)
         self.assertEqual(client.org, expected)
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
+
+    def test_public_repos_url(self):
+        """
+        Test that GithubOrgClient._public_repos_url returns the expected value.
+        """
+        payload = {
+            "repos_url": "https://api.github.com/orgs/test_org/repos"
+        }
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock) as mock:
+            mock.return_value = payload
+            client = GithubOrgClient("test_org")
+            result = client._public_repos_url
+
+            mock.assert_called_once()
+            self.assertEqual(result, payload["repos_url"])
