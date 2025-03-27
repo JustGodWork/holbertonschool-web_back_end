b5819a9a316268bd6dc593c266af7702f8cca27f
JustGodWork <discord1elie@gmail.com>
2025-03-27 11:53:09 +0100

    Add unit test for GithubOrgClient.public_repos method in test_client.py

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -45,3 +45,25 @@ class TestGithubOrgClient(unittest.TestCase):
 
             mock.assert_called_once()
             self.assertEqual(result, payload["repos_url"])
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """
+            Test that GithubOrgClient.public_repos
+            returns the expected list of repos.
+        """
+        url = "https://api.github.com/orgs/test_org/repos"
+        mock_get_json.return_value = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"}
+        ]
+        with patch.object(GithubOrgClient, "_public_repos_url",
+                          new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = url
+            client = GithubOrgClient("test_org")
+            result = client.public_repos()
+
+            self.assertEqual(result, ["repo1", "repo2", "repo3"])
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(url)
