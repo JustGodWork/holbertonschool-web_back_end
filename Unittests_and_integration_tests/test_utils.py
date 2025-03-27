125ccfb1c21d903492628d0037eb1969883085c7
JustGodWork <discord1elie@gmail.com>
2025-03-27 11:41:25 +0100

    Add unit tests for utils.memoize function and update test_utils.py documentation

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Unit tests for utils.access_nested_map and utils.get_json functions.
+Unit tests for utils.access_nested_map,
+utils.get_json, and utils.memoize functions.
 """
 
 import unittest
@@ -8,6 +9,7 @@ from parameterized import parameterized
 from utils import access_nested_map
 from unittest.mock import patch, Mock
 from utils import get_json
+from utils import memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -64,3 +66,30 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test case for the memoize decorator.
+    """
+
+    def test_memoize(self):
+        """
+        Test that memoize caches the result of a method call.
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method",
+                          return_value=42) as mock_method:
+            test_instance = TestClass()
+            # Call a_property twice
+            self.assertEqual(test_instance.a_property, 42)
+            self.assertEqual(test_instance.a_property, 42)
+            # Ensure a_method is only called once
+            mock_method.assert_called_once()
