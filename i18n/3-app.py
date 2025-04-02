1581e3242d32d8f733da6db9d766a28055b54f2d
JustGodWork <discord1elie@gmail.com>
2025-04-02 16:48:24 +0200

    Add initial Flask Babel app with i18n support and templates

---
+++ i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Flask Babel app with template parameterization """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """Configuration for Babel."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale():
+    """Determine the best match for supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """Render the index page."""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
