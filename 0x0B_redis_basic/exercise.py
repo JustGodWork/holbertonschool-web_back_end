f8fad59cea0ba1713a0d8957ed655ef1f13c6ee1
JustGodWork <discord1elie@gmail.com>
2025-04-09 14:21:54 +0200

    Add replay function to display history of function calls in Redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -55,6 +55,27 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable) -> None:
+    """
+    Display the history of calls of a particular function.
+
+    Args:
+        method: The method whose history is to be displayed.
+    """
+    redis_instance = method.__self__._redis
+    method_name = method.__qualname__
+    input_key = f"{method_name}:inputs"
+    output_key = f"{method_name}:outputs"
+
+    inputs = redis_instance.lrange(input_key, 0, -1)
+    outputs = redis_instance.lrange(output_key, 0, -1)
+
+    print(f"{method_name} was called {len(inputs)} times:")
+    for input_data, output_data in zip(inputs, outputs):
+        print(f"{method_name}(*{input_data.decode('utf-8')}) ->\
+               {output_data.decode('utf-8')}")
+
+
 class Cache:
     """Cache class to interact with Redis server."""
 
