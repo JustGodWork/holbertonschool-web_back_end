097450f430a4da65930d415124d2582c1d57b063
JustGodWork <discord1elie@gmail.com>
2025-04-09 14:16:35 +0200

    Add methods to retrieve data from Redis with optional conversion functions

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -28,3 +28,47 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis and optionally apply a conversion function.
+
+        Args:
+            key: The key to retrieve from Redis.
+            fn: An optional callable to convert the data.
+
+        Returns:
+            The retrieved data, optionally converted,
+            or None if the key does not exist.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve a string from Redis.
+
+        Args:
+            key: The key to retrieve.
+
+        Returns:
+            The retrieved string or None if the key does not exist.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve an integer from Redis.
+
+        Args:
+            key: The key to retrieve.
+
+        Returns:
+            The retrieved integer or None if the key does not exist.
+        """
+        return self.get(key, fn=int)
