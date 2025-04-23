7c2848be90cc26f427bf2eb2898d3135588603d1
JustGodWork <discord1elie@gmail.com>
2025-04-23 15:48:32 +0200

    Add SQL script to create users table with unique email and name

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Description: Create a table to store unique users with email and name
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
