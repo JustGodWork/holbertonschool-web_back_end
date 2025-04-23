e146269df33de51c9ca7bb36ff0e0e22db96e546
JustGodWork <discord1elie@gmail.com>
2025-04-23 15:57:25 +0200

    Add SQL procedure to compute the average score for a user

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,17 @@
+-- Description: Create a procedure to compute the average score for a user
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = user_id;
+
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END$$
+
+DELIMITER;
