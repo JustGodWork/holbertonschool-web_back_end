c83f08bf7dc54f1ec920d4e206780fb06bd953d1
JustGodWork <discord1elie@gmail.com>
2025-04-23 16:08:04 +0200

    Refactor procedure to compute average score for a user by simplifying the logic and removing unnecessary variable declaration

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -3,17 +3,12 @@ DELIMITER $$
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
-BEGIN
-    DECLARE avg_score FLOAT;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT) BEGIN
+UPDATE users
+SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+WHERE
+	id = user_id;
 
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
-END$$
-
-DELIMITER;
+END $$
+DELIMITER ;
