3223dee6f34325add0f7a744239e98330a142f81
JustGodWork <discord1elie@gmail.com>
2025-04-23 16:08:40 +0200

    Fix index creation syntax for first letter of 'name' column in index definition

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- Description: Create index on the first letter of the column 'name' and the column 'score'
 CREATE INDEX idx_name_first_score
-ON names (LEFT(name, 1), score);
+ON names (name (1), score)
