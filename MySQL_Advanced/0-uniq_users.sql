28d4ba2a719b081155a83a8c64ab064b33f2d885
JustGodWork <discord1elie@gmail.com>
2025-04-23 15:47:27 +0200

    Refactor SQL table definition for unique users by removing backticks from column names

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
+-- Description: Create a table to store unique users with email and name
 CREATE TABLE IF NOT EXISTS users (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
