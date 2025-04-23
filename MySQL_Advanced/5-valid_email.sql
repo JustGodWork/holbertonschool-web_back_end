518919aa75d1c137958e138be651bdc33465ade6
JustGodWork <discord1elie@gmail.com>
2025-04-23 15:53:55 +0200

    Add SQL trigger to reset valid_email when email is updated

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- Description: Create a trigger to reset valid_email when email is updated
+DELIMITER $$
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+DELIMITER;
