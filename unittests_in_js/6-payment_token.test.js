d70fe14b1f5ca4e4fd413acd1b5ad9863111666b
JustGodWork <discord1elie@gmail.com>
2025-05-14 23:34:09 +0200

    Add getPaymentTokenFromAPI function and corresponding tests

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+const { assert, expect } = require("chai");
+const getPaymentTokenFromAPI = require("./6-payment_token.js");
+
+describe("getPaymentTokenFromAPI", function () {
+  it("should return a resolved promise with correct data when success is true", function (done) {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        expect(response).to.deep.equal({
+          data: "Successful response from the API",
+        });
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
