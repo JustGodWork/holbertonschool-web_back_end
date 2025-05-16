26e483efbbaebf0f1fc90e27505ad3ffa2380054
JustGodWork <discord1elie@gmail.com>
2025-05-16 18:38:52 +0200

    Fix typo in test description and improve response body assertion for available payments endpoint

--- unittests_in_js/10-api/api.test.js
+++ unittests_in_js/10-api/api.test.js
@@ -20,23 +20,31 @@ describe("indexPageTest", function () {
     });
   });
 
-  it("Should work as expected for endpoint GET /available_payements", function (done) {
+  it("Should work as expected for endpoint GET /available_payments", function (done) {
     request.get("http://localhost:7865/available_payments", (error, response, body) => {
       chai.expect(response.statusCode).to.equal(200);
-      chai.expect(body).to.deep.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+      chai.expect(JSON.parse(body)).to.deep.equal({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      });
       if (error) return done(error);
       done();
     });
-  })
+  });
 
   it("should work as expected with POST /login endpoint", function (done) {
-    request.post("http://localhost:7865/login", { json: { "userName": "Betty" } }, (error, response, body) => {
+    request.post({
+      url: "http://localhost:7865/login",
+      json: { "userName": "Betty" }
+    }, (error, response, body) => {
       chai.expect(response.statusCode).to.equal(200);
       chai.expect(body).to.equal("Welcome Betty");
       if (error) return done(error);
       done();
     });
-  })
+  });
 
   it("Should return 404 when id is not numeric", function (done) {
     request.get("http://localhost:7865/cart/hello", (error, response, body) => {
