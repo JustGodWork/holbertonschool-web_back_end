ede921e7fa3a672e9634fd27cd093c88a060eae1
JustGodWork <discord1elie@gmail.com>
2025-05-16 18:38:47 +0200

    Refactor API routes for consistent formatting and improved readability

--- unittests_in_js/10-api/api.js
+++ unittests_in_js/10-api/api.js
@@ -2,30 +2,31 @@ const express = require("express");
 const port = 7865;
 
 const app = express();
+app.use(express.json());
 
-app.get("/", ( res) => {
-	res.send("Welcome to the payment system");
+app.get("/", (req, res) => {
+  res.send("Welcome to the payment system");
 });
 
 app.get("/cart/:id(\\d+)", (req, res) => {
-	res.status(200).send("Payment methods for cart " + req.params.id);
+  res.status(200).send("Payment methods for cart " + req.params.id);
 });
 
 app.get("/available_payments", (req, res) => {
-	res.status(200).send({
-		payment_methods: {
-			credit_cards: true,
-			paypal: false
-		}
-	});
+  res.status(200).json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  });
 });
 
 app.post("/login", (req, res) => {
-	res.status(200).send("Welcome " + req.body.userName);
+  res.status(200).send("Welcome " + req.body.userName);
 });
 
 app.listen(port, () => {
-	console.log("API available on localhost port " + port);
+  console.log("API available on localhost port " + port);
 });
 
 module.exports = app;
