866ab0d9b87dccd89ad90ab008c9f7c5c36c1cbe
JustGodWork <discord1elie@gmail.com>
2025-05-16 18:30:53 +0200

    Fix formatting in sendPaymentRequestToApi function and add missing newline at end of file

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -1,6 +1,6 @@
 const Utils = require('./utils');
 
-function sendPaymentRequestToApi (totalAmount, totalShipping) {
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${result}`);
 }
