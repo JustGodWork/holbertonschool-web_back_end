e9b1b1a69c4f0c3041042bbb81aafcdd89d40b23
JustGodWork <discord1elie@gmail.com>
2025-05-23 21:42:45 +0200

    Add function to create push notification jobs in the queue

---
+++ queuing_system_in_js/8-job.js
@@ -0,0 +1,19 @@
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) throw new Error('Jobs is not an array');
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData).save((err) => {
+      if (!err) console.log(`Notification job created: ${job.id}`);
+    });
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+    job.on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    });
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
+}
+
+export default createPushNotificationsJobs;
