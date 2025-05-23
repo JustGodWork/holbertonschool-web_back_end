bb0e23c057b0a7912ea6edd73633287ad85b0b1c
JustGodWork <discord1elie@gmail.com>
2025-05-23 21:41:38 +0200

    Add Redis client implementation with basic set and get operations

---
+++ queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,27 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.message);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+const getAsync = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  const reply = await getAsync(schoolName);
+  console.log(reply);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
