5b41da788b3f586e9bdfb753229faf06bdd41ac7
JustGodWork <discord1elie@gmail.com>
2025-05-23 21:42:00 +0200

    Add Redis client implementation with hash set and get operations

---
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,26 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.message);
+});
+
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+client.hgetall('HolbertonSchools', (err, obj) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(obj);
+});
