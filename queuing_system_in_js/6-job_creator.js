f09ce5233ded4f36fe7749041d72e1dc1627b10d
JustGodWork <discord1elie@gmail.com>
2025-05-23 21:42:26 +0200

    Add job creator for push notification using Kue

---
+++ queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
+};
+
+const job = queue.create('push_notification_code', jobData).save((err) => {
+  if (!err) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
